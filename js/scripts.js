ccf63d2f7c13a88c658bda8a99942b247504af6a
Stewart ilondanga <stilondanga@gmail.com>
2019-02-24 17:56:08 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,69 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+
+/*
+const Blob = document.querySelector('#blob');
+
+const topLeft = document.querySelector('#topLeft');
+topLeft.addEventListener('input', e => {
+  Blob.style.setProperty('--topLeft', topLeft.value);
+});
+
+const topLeft2 = document.querySelector('#topLeft2');
+topLeft2.addEventListener('input', e => {
+  Blob.style.setProperty('--topLeft2', topLeft2.value);
+});
+
+const topRight = document.querySelector('#topRight');
+topRight.addEventListener('input', e => {
+  Blob.style.setProperty('--topRight', topRight.value);
+});
+
+const topRight2 = document.querySelector('#topRight2');
+topRight2.addEventListener('input', e => {
+  Blob.style.setProperty('--topRight2', topRight2.value);
+});
+
+const bottomRight = document.querySelector('#bottomRight');
+bottomRight.addEventListener('input', e => {
+  Blob.style.setProperty('--bottomRight', bottomRight.value);
+});
+
+const bottomRight2 = document.querySelector('#bottomRight2');
+bottomRight2.addEventListener('input', e => {
+  Blob.style.setProperty('--bottomRight2', bottomRight2.value);
+});
+
+const bottomLeft = document.querySelector('#bottomLeft');
+bottomLeft.addEventListener('input', e => {
+  Blob.style.setProperty('--bottomLeft', bottomLeft.value);
+});
+
+const bottomLeft2 = document.querySelector('#bottomLeft2');
+bottomLeft2.addEventListener('input', e => {
+  Blob.style.setProperty('--bottomLeft2', bottomLeft2.value);
+});
+
+const blobHeight = document.querySelector('#height');
+blobHeight.addEventListener('input', e => {
+  Blob.style.setProperty('--blobHeight', blobHeight.value);
+});
+
+const blobWidth = document.querySelector('#width');
+blobWidth.addEventListener('input', e => {
+  Blob.style.setProperty('--blobWidth', blobWidth.value);
+});
+
+
+$( '.radius' ).change(function() {
+  var radius = $("#blob").css( "border-radius" );
+  $( "#result" ).html( "Border Radius: <span style='color:" +
+    radius + ";'>" + radius + "</span>" );
+});
